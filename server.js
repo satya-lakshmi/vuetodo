0f1ca4f1c647b86803da43e1564a8f9a87a3c95b
glakshmi-nyros <glakshmi.nyros@gmail.com>
2020-09-14 17:42:30 +0530

    this commit

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express  = require('express');
-const port = process.env.port || 8080;
+const express = require("express");
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static( dirname + "/dist/"));
-app.get(/.*/, function(req, res){
-	res.sendfile(__dirname + "/dist/index.html");
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
 });
 app.listen(port);
-console.log("server started....");
+
+console.log("Server started...");
